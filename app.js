1f12d9763f80d0cbfda2739e05078a49cd0c85da
James Wimmers <jimmyskydive@gmail.com>
2024-03-12 23:52:06 +1300

    added more functions to gameboard

--- app.js
+++ app.js
@@ -12,22 +12,62 @@ const player = (function () {
 })();
 const gameboard = (function () {
   let emptyPositions = [
-    [1, 1][1, 2][1, 3][(2, 1)][(2, 2)][(2, 3)][(3, 1)][(3, 2)][(3, 3)],
+    [1, 1],
+    [1, 2],
+    [1, 3],
+    [2, 1],
+    [2, 2],
+    [2, 3],
+    [3, 1],
+    [3, 2],
+    [3, 3],
   ];
+
   function checkDraw(emptyPositions) {
     if ((emptyPositions.length = 0)) {
-      return "draw";
+      return console.log("draw");
     }
   }
-  function checkWin(array) {
-    const counter = {};
-    array.forEach(element => {if (element[0])
-        
-    });
+  checkDraw(emptyPositions);
+
+  function countArray(array) {
+    const countX = {};
+    const countY = {};
+    for (let i = 0; i < array.length; i++) {
+      if (countX[array[i][0]]) {
+        countX[array[i][0]] += 1;
+      } else countX[array[i][0]] = 1;
+    }
+    for (let i2 = 0; i2 < array.length; i2++) {
+      if (countY[array[i2][1]]) {
+        countY[array[i2][1]] += 1;
+      } else countY[array[i2][1]] = 1;
+    }
+    console.log(countX);
+    console.log(countY);
+    checkWin(countX, countY);
   }
+  function checkWin(countX, countY) {
+    console.log(Object.values(countX));
+    console.log(Object.values(countY));
+    if (Object.values(countX).find((element) => element == 3))
+      console.log("win");
+    else if (Object.values(countY).find((element) => element == 3))
+      console.log("win");
+  }
+  countArray([
+    [2, 1],
+    [1, 3],
+    [2, 2],
+  ]);
 })();
 
-//3 positions in a line = win
-//cannot place a tile on existing tile
+/*
+for diagonal victories has each number 1,2 & 3 twice = win
 
-//reset with each match (overall score maybe)
+computer logic for picking a random slot
+cannot place a tile on existing tile
+reset with each match
+*/
+
+
