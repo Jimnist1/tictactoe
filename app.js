643a36c4d8cfd91c0c422e2486c68c44c5921c35
James Wimmers <jimmyskydive@gmail.com>
2024-04-24 04:55:31 +1200

    ai logic added however not perfectly implemented

--- app.js
+++ app.js
@@ -4,8 +4,14 @@ const initialState = {
   computer: {
     name: "Computer",
     token: "",
+    difficulty: "easy",
     positions: [],
     active: true,
+    board: [
+      ["", "", ""],
+      ["", "", ""],
+      ["", "", ""],
+    ],
   },
   player2: {
     name: "Player 2",
@@ -39,124 +45,152 @@ let currentState = { ...initialState };
 
 //Objects
 const computer = (function () {
-  let { name, token, positions, active } = currentState.computer;
-  let difficulty = document.getElementById("difficultyChoice");
+  let { name, token, difficulty, positions, active, board } =
+    currentState.computer;
   //AI Minmax Logic
-  function minimax(board, depth, maximizingPlayer) {
-    let result = evaluate(board);
-    if (result !== null) {
-      return result;
+  function fillBoard(turnTaker) {
+    let currentPosition = turnTaker.positions;
+    if (currentPosition != null) {
+      currentPosition.forEach((element) => {
+        computer.board[element[0] - 1][element[1] - 1] = turnTaker.token;
+      });
     }
-
-    if (maximizingPlayer) {
+    console.log(computer.board);
+  }
+  function bestMove() {
+    let bestScore = -Infinity;
+    let move;
+    for (let i = 0; i < 3; i++) {
+      for (let j = 0; j < 3; j++) {
+        if (computer.board[i][j] == "") {
+          computer.board[i][j] = computer.token;
+          let score = minimax(computer.board, 0, false);
+          computer.board[i][j] = "";
+          if (score > bestScore) {
+            bestScore = score;
+            move = { i, j };
+          }
+        }
+      }
+    }
+    console.log(move);
+    console.log(move.i + 1 + "," + (move.j + 1));
+    return [move.i + 1, move.j + 1];
+  }
+  let aiScores = {
+    X: 1,
+    O: -1,
+    tie: 0,
+  };
+  function minimax(board, depth, isMaximizing) {
+    let result = aiCheckWinner();
+    if (result != null) {
+      return aiScores[result];
+    }
+    if (isMaximizing) {
       let bestScore = -Infinity;
-      for (let i = 0; i < board.length; i++) {
-        for (let j = 0; j < board[i].length; j++) {
-          if (board[i][j] === "") {
-            board[i][j] = "X";
-            let score = minimax(board, depth + 1, false);
-            board[i][j] = "";
-            bestScore = Math.max(bestScore, score);
+      for (let i = 0; i < 3; i++) {
+        for (let j = 0; j < 3; j++) {
+          if (board[i][j] == "") {
+            board[i][j] = computer.token;
+            let score = minimax(computer.board, depth + 1, false);
+            computer.board[i][j] = "";
+            bestScore = Math.max(score, bestScore);
           }
         }
       }
       return bestScore;
     } else {
       let bestScore = Infinity;
-      for (let i = 0; i < board.length; i++) {
-        for (let j = 0; j < board[i].length; j++) {
-          if (board[i][j] === "") {
-            board[i][j] = "O";
-            let score = minimax(board, depth + 1, true);
+      for (let i = 0; i < 3; i++) {
+        for (let j = 0; j < 3; j++) {
+          if (board[i][j] == "") {
+            board[i][j] = player.token;
+            let score = minimax(computer.board, depth + 1, true);
             board[i][j] = "";
-            bestScore = Math.min(bestScore, score);
+            bestScore = Math.min(score, bestScore);
           }
         }
       }
       return bestScore;
     }
   }
-  function findBestMove(board) {
-    let bestScore = -Infinity;
-    let bestMove = [row, column];
+  function equalsThree(a, b, c) {
+    return a == b && b == c && a != "";
+  }
+  function aiCheckWinner() {
+    let winner = null;
+    for (let i = 0; i < 3; i++) {
+      if (
+        equalsThree(
+          computer.board[i][0],
+          computer.board[i][1],
+          computer.board[i][2]
+        )
+      ) {
+        winner = computer.board[i][0];
+      }
+    }
+    for (let i = 0; i < 3; i++) {
+      if (
+        equalsThree(
+          computer.board[0][i],
+          computer.board[1][i],
+          computer.board[2][i]
+        )
+      ) {
+        winner = computer.board[0][i];
+      }
+    }
+    for (let i = 0; i < 3; i++) {
+      if (
+        equalsThree(
+          computer.board[0][0],
+          computer.board[1][1],
+          computer.board[2][2]
+        )
+      ) {
+        winner = computer.board[i][0];
+      }
+    }
+    for (let i = 0; i < 3; i++) {
+      if (
+        equalsThree(
+          computer.board[2][0],
+          computer.board[1][1],
+          computer.board[0][2]
+        )
+      ) {
+        winner = computer.board[2][0];
+      }
+    }
+    let openSpots = 0;
     for (let i = 0; i < 3; i++) {
       for (let j = 0; j < 3; j++) {
-        if (board[i][j] === "") {
-          board[i][j] = "X";
-          const score = minimax(board, 0, false);
-          board[i][j] = "";
-          if (score > bestScore) {
-            bestScore = score;
-            row = i;
-            column = j;
-          }
+        if (computer.board[i][j] == "") {
+          openSpots++;
         }
       }
     }
-    return bestMove;
+    if (winner == null && openSpots == 0) {
+      return "tie";
+    } else return winner;
   }
-  function evaluate(board) {
-    const winningCombinations = [
-      [board[0][0], board[0][1], board[0][2]],
-      [board[1][0], board[1][1], board[1][2]],
-      [board[2][0], board[2][1], board[2][2]],
-      [board[0][0], board[1][0], board[2][0]],
-      [board[0][1], board[1][1], board[2][1]],
-      [board[0][2], board[1][2], board[2][2]],
-      [board[0][0], board[1][1], board[2][2]],
-      [board[0][2], board[1][1], board[2][0]],
-    ];
 
-    for (const combination of winningCombinations) {
-      if (combination.every((cell) => cell === "X")) {
-        return 10;
-      } else if (combination.every((cell) => cell === "O")) {
-        return -10;
-      }
-    }
-    if (board.every((row) => row.every((cell) => cell !== ""))) {
-      return 0;
-    }
-    return 5;
-  }
-  board = [
-    ["", "", ""],
-    ["", "", ""],
-    ["", "", ""],
-  ];
-  const bestScore = minimax(board, 0, true);
-  console.log("Best score:", bestScore);
-  /*const difficulty = (function () {
-  EASY: 'easy',
-  MODERATE: 'moderate',
-  HARD: 'hard';
-// Function to set AI difficulty
-    (difficulty) => {
-      switch (difficulty) {
-        case difficulty.EASY:
-          // Set AI strategy for easy difficulty
-          aiStrategy = randomMove;
-          break;
-        case difficulty.MODERATE:
-          // Set AI strategy for moderate difficulty
-          aiStrategy = moderateMove;
-          break;
-        case difficulty.HARD:
-          // Set AI strategy for hard difficulty
-          aiStrategy = hardMove;
-          break;
-        default:
-          // Default to easy difficulty if invalid difficulty is provided
-          aiStrategy = randomMove;
-      }
-    }
-})();
-*/
   function randomIndexPos(gameboardLength) {
     return Math.floor(Math.random() * gameboardLength);
   }
-
-  return { name, token, positions, active, randomIndexPos };
+  return {
+    name,
+    token,
+    difficulty,
+    positions,
+    active,
+    board,
+    randomIndexPos,
+    fillBoard,
+    bestMove,
+  };
 })();
 
 const player = (function () {
@@ -177,6 +211,7 @@ const gameboard = (function () {
   const outcomeMessage = document.getElementById("endGameMessage");
   const winLine = document.querySelector(".winLine");
   const gameboardContainer = document.querySelector(".gameboardContainer");
+  const difficultyStatus = document.getElementById("difficultyChoice");
 
   //Bind Events
   document.addEventListener("click", function (buttonPressed) {
@@ -200,6 +235,12 @@ const gameboard = (function () {
     }
   });
   document.addEventListener("DOMContentLoaded", function () {
+    difficultyStatus.addEventListener("change", function () {
+      if (difficultyStatus.value) {
+        computer.difficulty = difficultyStatus.value;
+        resetGame();
+      }
+    });
     symbolSelect.addEventListener("change", function () {
       player.token = symbolSelect.value;
       computer.token = getAlternateCounter(player.token);
@@ -210,9 +251,11 @@ const gameboard = (function () {
       if (opponent.value == "computer") {
         computer.active = true;
         player2.active = false;
+        difficultyStatus.disabled = false;
       } else if (opponent.value == "player") {
         computer.active = false;
         player2.active = true;
+        difficultyStatus.disabled = true;
       }
       resetGame();
     });
@@ -230,27 +273,55 @@ const gameboard = (function () {
   function toggleProperty(obj, property) {
     obj[property] = !obj[property]; // Invert the boolean value of the property
   }
-  function randomMove() {
+  function compMove() {
     if (emptyPositions.length > 0) {
-      randomIndex = computer.randomIndexPos(emptyPositions.length);
-      if (randomIndex !== -1) {
-        const removedPosition = emptyPositions.splice(randomIndex, 1)[0];
-        computer.positions.push(removedPosition);
-        setTimeout(() => {
-          placeCounter(computer.positions, computer.token);
-        }, 400);
-      }
+      aiDescision();
     }
+  }
+  function aiDescision() {
+    let currentStrategy = computer.difficulty;
+    switch (currentStrategy) {
+      case "easy":
+        randomMove();
+        break;
+      case "moderate":
+        let choice = computer.randomIndexPos(2);
+        if (choice == 1) randomMove();
+        else if (choice == 2) aiMove();
+        break;
+      case "hard":
+        aiMove();
+        break;
+    }
+  }
+  function randomMove() {
+    randomIndex = computer.randomIndexPos(emptyPositions.length);
+    if (randomIndex !== -1) {
+      const removedPosition = emptyPositions.splice(randomIndex, 1)[0];
+      computer.positions.push(removedPosition);
+      setTimeout(() => {
+        gameboard.placeCounter(computer.positions, computer.token);
+      }, 400);
+    }
+  }
+  function aiMove() {
+    position = computer.bestMove();
+    console.log(position);
+    setTimeout(() => {
+      moveArray(position, computer);
+    }, 400);
   }
   function playerVsComputerEvent(position) {
     if (checkForCounter(position) == false) {
       moveArray(position, player);
       countArray(player);
+      computer.fillBoard(player);
       if (gameComplete == false) {
         checkDraw(emptyPositions);
-        randomMove();
+        compMove();
         countArray(computer);
         checkDraw(emptyPositions);
+        computer.fillBoard(computer);
       }
     }
   }
@@ -389,19 +460,19 @@ const gameboard = (function () {
     return (isPositionFound =
       player.positions.find(
         (position) =>
-          position[0] === positionToFind[0] && position[1] === positionToFind[1]
+          position[0] == positionToFind[0] && position[1] == positionToFind[1]
       ) !== undefined);
   }
   function checkWin(countX, countY, player) {
     if (Object.values(countX).find((element) => element == 3)) {
-      const objectKey = Object.keys(countX).find((key) => countX[key] === 3);
+      const objectKey = Object.keys(countX).find((key) => countX[key] == 3);
       adjustWinLinePosition(objectKey, 1, 60, 0);
       disablePointerEvents();
       startWinLineAnimation("horizontal");
       displayOutcome(player, "horizontal");
       gameComplete = true;
     } else if (Object.values(countY).find((element) => element == 3)) {
-      const objectKey = Object.keys(countY).find((key) => countY[key] === 3);
+      const objectKey = Object.keys(countY).find((key) => countY[key] == 3);
       adjustWinLinePosition(1, objectKey, -100, 150);
       disablePointerEvents();
       startWinLineAnimation("vertical");
@@ -484,6 +555,11 @@ const gameboard = (function () {
     player.turn = initialState.player.turn;
     player2.turn = initialState.player2.turn;
     gameComplete = false;
+    computer.board = [
+      ["", "", ""],
+      ["", "", ""],
+      ["", "", ""],
+    ];
     stopWinLineAnimation();
     enablePointerEvents();
     currentState.gameboard.emptyPositions = [
@@ -497,12 +573,13 @@ const gameboard = (function () {
         gameCounter.textContent = ""; // Set text content to empty
       }
     });
-    if (computer.token == "X" && computer.active == true) randomMove();
+    if (computer.token == "X" && computer.active == true) compMove();
   }
   return {
     emptyPositions,
     symbolSelect,
     getAlternateCounter,
+    placeCounter,
   };
 })();
 
